3bbba5acc6e38d6a3efacc9302d35d2f39fa6fd8
SpikeHimself <spike@spikehimself.ovh>
2023-02-01 20:36:15 +0100

    Post-build automation fixes

--- publish.ps1
+++ publish.ps1
@@ -23,12 +23,15 @@ if ($DeployPath.Equals("") -Or $DeployPath.Equals("Build")){
     $DeployPath = "$ValheimPath\BepInEx\plugins"
 }
 
-Write-Host	"Target:          $Target"
-Write-Host	"TargetPath:      $TargetPath"
-Write-Host	"TargetAssembly:  $TargetAssembly"
-Write-Host	"ValheimPath:     $ValheimPath"
-Write-Host	"ProjectPath:     $ProjectPath"
-Write-Host	"DeployPath:      $DeployPath"
+$PrePackagePath = "$ProjectPath\Prepackage"
+
+Write-Host "Target:          $Target"
+Write-Host "TargetPath:      $TargetPath"
+Write-Host "TargetAssembly:  $TargetAssembly"
+Write-Host "ValheimPath:     $ValheimPath"
+Write-Host "ProjectPath:     $ProjectPath"
+Write-Host "DeployPath:      $DeployPath"
+Write-Host "PrePackagePath:  $PrePackagePath"
 
 # Make sure Get-Location is the script path
 Push-Location -Path (Split-Path -Parent $MyInvocation.MyCommand.Path)
@@ -54,20 +57,18 @@ if (Test-Path -Path "$pdb") {
 # Main Script
 Write-Host "Publishing for $Target from $TargetPath"
 
-if ($Target.Equals("Debug")) {
-    if ($DeployPath.Equals("") -Or $DeployPath.Equals("Build")){
-      $DeployPath = "$ValheimPath\BepInEx\plugins"
-    }
-
+if ($Target.Equals("Debug")) 
+{
     $PluginPath = New-Item -Type Directory -Path "$DeployPath\$name" -Force
     Write-Host "Copy $TargetAssembly to $PluginPath"
     Copy-Item -Path "$TargetPath\$name.dll" -Destination "$PluginPath" -Force
     Copy-Item -Path "$TargetPath\$name.pdb" -Destination "$PluginPath" -Force
     Copy-Item -Path "$TargetPath\$name.dll.mdb" -Destination "$PluginPath" -Force
-    Copy-Item -Path "$ProjectPath\PrePackage\Translations" -Destination "$PluginPath\" -Recurse -Force
+    Copy-Item -Path "$PrePackagePath\Translations" -Destination "$PluginPath\" -Recurse -Force
 }
 
-if($Target.Equals("Release")) {
+if($Target.Equals("Release")) 
+{
     Write-Host "Packaging for ThunderStore..."
     $Package="Package"
     $PackagePath="$ProjectPath\$Package"
@@ -75,14 +76,17 @@ if($Target.Equals("Release")) {
     Write-Host "$PackagePath\$TargetAssembly"
     $PackagePluginsPath = New-Item -Type Directory -Path "$PackagePath\plugins\$name" -Force
     $PackageReleasePath = New-Item -Type Directory -Path "$ProjectPath\Release" -Force
+    
 
     Copy-Item -Path "$TargetPath\$TargetAssembly" -Destination "$PackagePluginsPath\" -Force
-    Copy-Item -Path "$ProjectPath\Prepackage\Translations" -Destination "$PackagePluginsPath\" -Recurse -Force
-    Copy-Item -Path "$ProjectPath\README.md" -Destination "$PackagePath\" -Force
-    Copy-Item -Path "$ProjectPath\PrePackage\manifest.json" -Destination "$PackagePath\" -Force
+    Copy-Item -Path "$PrePackagePath\Translations" -Destination "$PackagePluginsPath\" -Recurse -Force
+    Copy-Item -Path "$PrePackagePath\README.md" -Destination "$PackagePath\" -Force
+    Copy-Item -Path "$PrePackagePath\manifest.json" -Destination "$PackagePath\" -Force
     Copy-Item -Path "$ProjectPath\..\Images\icon.png" -Destination "$PackagePath\" -Force
     
-    Compress-Archive -Path "$PackagePath\*" -DestinationPath "$PackageReleasePath\$name.zip" -Force
+    $CompressedOutputFilename = "$PackageReleasePath\$name-new.zip"
+    Compress-Archive -Path "$PackagePath\*" -DestinationPath "$CompressedOutputFilename" -Force
+    Write-Host "Package ready: $CompressedOutputFilename"
 }
 
 # Pop Location
