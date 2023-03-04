566dc039e8ca3a4c8ab681026e4b9145c05f5d54
SpikeHimself <spike@spikehimself.ovh>
2023-03-04 20:56:33 +0100

    Add config option 'DisplayPortalColour'

--- XPortal/XPortalConfig.cs
+++ XPortal/XPortalConfig.cs
@@ -19,6 +19,7 @@ namespace XPortal
         public class ConfigSettings
         {
             public bool PingMapDisabled;
+            public bool DisplayPortalColour;
         }
 
         /// <summary>
@@ -44,65 +45,52 @@ namespace XPortal
         public void LoadConfigFile(ConfigFile config)
         {
             configFile = config;
+            ReloadLocalConfig();
 
-            ReloadConfig();
+            configFile.ConfigReloaded += LocalConfigChanged;
+            configFile.SettingChanged += LocalConfigChanged;
 
-            configFile.ConfigReloaded += ConfigFile_ConfigReloaded;
-            configFile.SettingChanged += Config_SettingChanged;
+            if (XPortal.IsServer())
+            {
+                Server = Local;
+            }
         }
 
         /// <summary>
         /// Reload the settings inside the config file
         /// </summary>
-        private void ReloadConfig()
+        private void ReloadLocalConfig()
         {
             // Add Nexus ID to config for Nexus Update Check (https://www.nexusmods.com/valheim/mods/102)
             configFile.Bind<int>("General", "NexusID", XPortal.PluginNexusId, "Nexus mod ID for updates (do not change)");
 
             // Add PingMapDisabled option which disables the Ping Map button
-            var cfgPingMapDisabled = configFile.Bind<bool>("General", "PingMapDisabled", false, "Disable the Ping Map button completely. For players who wish to play without a map.");
+            var cfgPingMapDisabled = configFile.Bind<bool>("General", "PingMapDisabled", false, "Disable the Ping Map button completely. For players who wish to play without a map. This setting is enforced (but not overwritten) by the server.");
             Local.PingMapDisabled = cfgPingMapDisabled.Value;
-            Server.PingMapDisabled = cfgPingMapDisabled.Value;
 
+            var cfgDisplayPortalColour = configFile.Bind<bool>("General", "DisplayPortalColour", false, "Show a \">>\" tag in the list of portals that has the same colour as the light that the portal emits (integration with \"Advanced Portals\" by RandyKnapp).");
+            Local.DisplayPortalColour = cfgDisplayPortalColour.Value;
         }
 
         /// <summary>
         /// The config file was reloaded on the server. Read settings from it and send them to clients.
         /// </summary>
-        private void ConfigFile_ConfigReloaded(object sender, EventArgs e)
+        private void LocalConfigChanged(object sender, EventArgs e)
         {
-            if (!XPortal.IsServer())
+            ReloadLocalConfig();
+
+            if (XPortal.IsServer())
             {
-                return;
+                Jotunn.Logger.LogDebug("The config was changed, propagating to clients..");
+                RPC.SendConfigToClients(PackLocalConfig());
             }
-
-            ReloadConfig();
-
-            Jotunn.Logger.LogDebug("[ConfigFile_ConfigReloaded] The config file was reloaded, propagating to clients..");
-            RPC.SendConfigToClients(Pack());
-        }
-
-        /// <summary>
-        /// One of the settings was changed on the server. Read settings from the config file and send them to clients.
-        /// </summary>
-        private void Config_SettingChanged(object sender, SettingChangedEventArgs e)
-        {
-            if (!XPortal.IsServer())
-            {
-                return;
-            }
-
-            ReloadConfig();
-
-            Jotunn.Logger.LogDebug("[Config_SettingChanged] A config value has changed, propagating to clients..");
-            RPC.SendConfigToClients(Pack());
         }
 
         /// <summary>
         /// Wrap the config settings into a package
         /// </summary>
         /// <returns>A ZPackage containing all config settings</returns>
-        public ZPackage Pack()
+        public ZPackage PackLocalConfig()
         {
             var pkg = new ZPackage();
             pkg.Write(Local.PingMapDisabled);
@@ -113,7 +101,7 @@ namespace XPortal
         /// Set our config settings based on the package we received from the server
         /// </summary>
         /// <param name="pkg">A ZPackage containing all config settings</param>
-        public void ReceiveFromServer(ZPackage pkg)
+        public void ReceiveServerConfig(ZPackage pkg)
         {
             Server.PingMapDisabled = pkg.ReadBool();
             Jotunn.Logger.LogDebug($"[ReceiveFromServer] PingMapDisabled {{ Local: {Local.PingMapDisabled}, Server: {Server.PingMapDisabled} }}");
