9ce1bb1b6440c4ef3214879a12bc6703cd307bba
SpikeHimself <spike@spikehimself.ovh>
2023-03-04 21:15:03 +0100

    A first attempt at modular documentation using ManyFormats and T4 templating

---
+++ XPortal/PrePackage/Readmes/ReadmeHelper.cs
@@ -0,0 +1,28 @@
+﻿using ManyFormats;
+
+namespace XPortal.PrePackage.Readmes
+{
+    public enum Targets
+    {
+        Thunderstore,
+        NexusMods,
+        GitHub,
+    }
+
+    public class NexusBbcode : ManyFormats.Formats.Bbcode
+    {
+        public NexusBbcode() : base("BBCode for Nexus Mods") { }
+
+        public override string Heading(string text, HeadingSize size = HeadingSize.Largest)
+        {
+            return base.Heading(Colour(text, "#FFA03C"), size);
+        }
+
+        public override string Code(string text, CodeMode mode = CodeMode.Inline)
+        {
+            return (mode == CodeMode.Inline)
+                ? Bold("\"" + text + "\"")
+                : base.Code(text, mode);
+        }
+    }
+}
