7497e95798ca00a40d6472936ac0eef52e827335
SpikeHimself <spike@spikehimself.ovh>
2023-03-14 01:36:32 +0100

    Update Jotun 2.11.0, BepInEx 5.4.21

--- XPortal/ModInfo.cs
+++ XPortal/ModInfo.cs
@@ -12,7 +12,7 @@
         public const string Description = "Select portal destination from a list of existing portals. No more tag pairing, and no more portal hubs! XPortal is a complete rewrite of the popular mod AnyPortal.";
         public const string WebsiteUrl = "https://github.com/" + GitHubRepo;
         public const int NexusId = 2239;
-        //public const string BepinVersion = ??
+        public const string BepInExPackVersion = "5.4.2100";
         public const string JotunnVersion = Jotunn.Main.Version;
     }
 }
