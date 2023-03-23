dcb0670cdb6d9f23b4323e176a572f59ecbd68f8
SpikeHimself <spike@spikehimself.ovh>
2023-03-23 23:32:29 +0100

    Prepare v1.2.10

--- XPortal/ModInfo.cs
+++ XPortal/ModInfo.cs
@@ -8,7 +8,7 @@
         public const string Author = "SpikeHimself";
         public const string Name = "XPortal";
         public const string GitHubRepo = "SpikeHimself/XPortal";
-        public const string Version = "1.2.9";
+        public const string Version = "1.2.10";
         public const string Description = "Select portal destination from a list of existing portals. No more tag pairing, and no more portal hubs! XPortal is a complete rewrite of the popular mod AnyPortal.";
         public const string WebsiteUrl = "https://github.com/" + GitHubRepo;
         public const int NexusId = 2239;
