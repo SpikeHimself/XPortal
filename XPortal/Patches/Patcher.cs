8ac8631576184976102b7bde419d7d6c40b7a341
Pete Navarra <vapokrocks@gmail.com>
2026-04-04 05:07:00 -0400

    Putting these comments back in.

--- XPortal/Patches/Patcher.cs
+++ XPortal/Patches/Patcher.cs
@@ -14,12 +14,30 @@ namespace XPortal.Patches
             patcher.PatchAll(typeof(Game_Start));
             patcher.PatchAll(typeof(Game_ConnectPortals));
             patcher.PatchAll(typeof(Game_ConnectPortalsCoroutine));
+            //patcher.PatchAll(typeof(Player_PlacePiece));
             patcher.PatchAll(typeof(TeleportWorld_GetHoverText));
             patcher.PatchAll(typeof(TextInput_RequestText));
             patcher.PatchAll(typeof(WearNTear_Destroy));
             patcher.PatchAll(typeof(ZDOMan_ConnectPortals));
             patcher.PatchAll(typeof(ZNet_RPC_PeerInfo_Postfix));
+
+            // Temporarily disable this work-around and revert back to the old method
+
             patcher.PatchAll(typeof(WearNTear_OnPlaced));
+
+            //var playerPlacePiecePatched =
+            //    Harmony.GetAllPatchedMethods().Where(m => m.DeclaringType.Name.Equals("Player") && m.Name.Equals("PlacePiece")).Any();
+
+            //if (!playerPlacePiecePatched)
+            //{
+            //    Log.Debug("Patching WearNTear.OnPlaced, yay!");
+            //    patcher.PatchAll(typeof(WearNTear_OnPlaced));
+            //}
+            //else
+            //{
+            //    Log.Debug("Patching Piece.SetCreator, boo!");
+            //    patcher.PatchAll(typeof(Piece_SetCreator));
+            //}
         }
 
         public static void Unpatch() => patcher?.UnpatchSelf();
